ccd0aa08955abef8d8cd9ca74e54481d332bc3e7
Asumarin <asumarin18@gmail.com>
2024-10-24 16:04:29 +0300

    catching nonexisting routes

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = new Application();
 
 app.use(jsonParser);
 app.use(parseUrl("http://localhost:5000"));
+app.use((req, res) => {
+  if ((res.statusCode = 404)) {
+    res.write(`Resourse ${req.url} not found`);
+    res.end();
+  }
+});
 
 app.addRouter(userRouter);
 
